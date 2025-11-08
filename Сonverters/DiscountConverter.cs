e167e86bcea627ed48441d1903fdd856185fe10a
KuvoKuvo <ip0804019@gmail.com>
2025-11-09 03:44:48 +0700

    Конверторы(проба)

---
+++ Сonverters/DiscountConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows;
+
+namespace MusicRental.Сonverters
+{
+    public class DiscountConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is decimal discount)) return null;
+
+            switch (parameter as string)
+            {
+                case "Color":
+                    return discount > 15 ?
+                        new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2E8B57")) :
+                        new SolidColorBrush(Colors.Black);
+
+                case "Decorations":
+                    return discount > 15 ? TextDecorations.Underline : null;
+
+                case "Visibility":
+                    return discount > 0 ? Visibility.Visible : Visibility.Collapsed;
+
+                default:
+                    return discount;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Сonverters/PriceConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+namespace MusicRental.Сonverters
+{
+    public class PriceConverter : IMultiValueConverter
+    {
+        public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Цена и скидка 
+            if (values.Length < 2 || !(values[0] is decimal price) || !(values[1] is decimal discount))
+                return null;
+
+            switch (parameter as string)
+            {
+                case "FinalPrice":
+                    var finalPrice = price * (1 - discount / 100);
+                    return finalPrice.ToString("F2");
+
+                case "PriceVisibility":
+                    return discount > 0 ? Visibility.Visible : Visibility.Collapsed;
+
+                case "PriceForeground":
+                    return discount > 0 ? Brushes.Red : Brushes.Black;
+
+                case "PriceDecorations":
+                    return discount > 0 ? TextDecorations.Strikethrough : null;
+
+                default:
+                    return price.ToString("F2");
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
